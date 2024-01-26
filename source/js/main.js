8e28be91b2e75ec4601b162168b71e88bc4943e1
Nate Swart <839888+nateswart@users.noreply.github.com>
2024-01-25 20:33:27 -0500

    Restored original menubar hiding feature

--- source/js/main.js
+++ source/js/main.js
@@ -42,7 +42,32 @@ $(document).ready(function() {
       return false;
     });
 
+    /**
+     * Add a scroll listener to the menu to hide/show the navigation links.
+     */
+    if (menu.length) {
+      $(window).on('scroll', function() {
+        var topDistance = $("#menu > #nav").offset().top;
 
+        // hide only the navigation links on desktop
+        if (menu.css('visibility') !== 'hidden' && topDistance < 50) {
+          $("#menu > #nav").show();
+        } else if (menu.css('visibility') !== 'hidden' && topDistance > 100) {
+          $("#menu > #nav").hide();
+        }
+
+        // on tablet, hide the navigation icon as well and show a "scroll to top
+        // icon" instead
+        if ( ! $( "#menu-icon" ).is(":visible") && topDistance < 50 ) {
+          $("#menu-icon-tablet").show();
+          $("#top-icon-tablet").hide();
+        } else if (! $( "#menu-icon" ).is(":visible") && topDistance > 100) {
+          $("#menu-icon-tablet").hide();
+          $("#top-icon-tablet").show();
+        }
+      });
+    }
+    
     /**
      * Show mobile navigation menu after scrolling upwards,
      * hide it again after scrolling downwards.
